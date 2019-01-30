7d304b51c355b2e1c8bac20987fdbf4f35feacac
ayan sil <ayansil.com@gmail.com>
2019-01-31 03:07:35 +0530

    cleared up

---
+++ src/app/pages/forms/customers/customers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.scss']
+})
+export class CustomersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
